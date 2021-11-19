c0536ac7d886345e8523bcac5d284a5aa7a8d3b1
laon-private <public@laonstory.com>
2021-11-19 18:48:05 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+gsap.to(".box1", { duration:3, x:200, opacity: 0.2, ease:'steps(10)', delay:2,});
+
+gsap.to(".box2", { duration:3, x:200, roate: 720, scale:1.3,});
+
+gsap.to('.box3', { duration:3, x:200, ease:'elastic', backgroundColor:'red', width:300, fontsize:60});
